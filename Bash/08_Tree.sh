52b8492f6ee61ccc1e586baae8639d45b24f4ccc
堀内幸太郎 <ani@horiuchikoutarounoMacBook-puro.local>
2022-09-29 10:02:01 +0900

    a

--- Bash/08_Tree.sh
+++ Bash/08_Tree.sh
@@ -3,60 +3,60 @@
 root="";
 ### Utility functions to generate a BST ###
 #スタックの関数を追加した。配列ごとに別メソッドで作ったがevalでまとめた方が良さそう
-function globalStack.push() {
-    globalarr+=($1)
+function Stack_global.push() {
+    Stack_global+=($1)
 }
 
-function globalStack.pop() {
-    if [ ${#globalarr[*]} == 0 ];then
+function Stack_global.pop() {
+    if [ ${#Stack_global[*]} == 0 ];then
       echo "null";
       return;
     fi
-    local el=${globalarr[${#globalarr[*]}-1]}
-    po=$((${#globalarr[*]}-1))
-    unset globalarr[$po]
+    local el=${Stack_global[${#Stack_global[*]}-1]}
+    po=$((${#Stack_global[*]}-1))
+    unset Stack_global[$po]
 }
-function globalStack.peek() {
-    if [ ${#globalarr[*]} == 0 ];then
+function Stack_global.peek() {
+    if [ ${#Stack_global[*]} == 0 ];then
       echo "null";
       return;
     fi
-    local el=${globalarr[${#globalarr[*]}-1]}
-    po=$((${#globalarr[*]}-1))
+    local el=${Stack_global[${#Stack_global[*]}-1]}
+    po=$((${#Stack_global[*]}-1))
     echo $el
 }
-function globalStack.isEmpty() {
-  if [ ${#globalarr[*]} == 0 ];then
+function Stack_global.isEmpty() {
+  if [ ${#Stack_global[*]} == 0 ];then
     echo "true";
   else
     echo "false"
   fi
 }
-function localStack.push() {
-    localarr+=($1)
+function Stack_local.push() {
+    Stack_local+=($1)
     #echo pushed: $1
 }
 
-function localStack.pop() {
-    if [ ${#localarr[*]} == 0 ];then
+function Stack_local.pop() {
+    if [ ${#Stack_local[*]} == 0 ];then
       echo "null";
       return;
     fi
-    local el=${localarr[${#localarr[*]}-1]}
-    po=$((${#localarr[*]}-1))
-    unset localarr[$po]
+    local el=${Stack_local[${#Stack_local[*]}-1]}
+    po=$((${#Stack_local[*]}-1))
+    unset Stack_local[$po]
 }
-function localStack.peek() {
-    if [ ${#localarr[*]} == 0 ];then
+function Stack_local.peek() {
+    if [ ${#Stack_local[*]} == 0 ];then
       echo "null";
       return;
     fi
-    local el=${localarr[${#localarr[*]}-1]}
-    po=$((${#localarr[*]}-1))
+    local el=${Stack_local[${#Stack_local[*]}-1]}
+    po=$((${#Stack_local[*]}-1))
     echo $el
 }
-function localStack.isEmpty() {
-  if [ ${#localarr[*]} == 0 ];then
+function Stack_local.isEmpty() {
+  if [ ${#Stack_local[*]} == 0 ];then
     echo "true";
   else
     echo "false"
@@ -81,52 +81,6 @@ gen_uid() {
 	# bash variable
 	echo "id$(uuidgen|tr -d '-')";
 }
-###
-function display_Tree(){
-
-globalarr=();
-globalStack.push "$root";
-nBlanks=32;
-isRowEmpty="false";
-echo "......................................................"
-while [[ $isRowEmpty == "false" ]];do
-  localarr=();
-  isRowEmpty="true"
-  for ((j=0; j<nBlanks; j++));do
-    echo -n " ";
-  done
-  while [[ $(globalStack.isEmpty) == "false" ]];do
-    #サブシェルでpopメソッドを呼び出しても配列の削除が反映されないので
-    #peekメソッドを別に作った。
-    temp=$(globalStack.peek);
-    globalStack.pop
-    if [ "$temp" != "null" ];then
-      echo -n $(${temp}.getValue)
-      localStack.push $(eval ${temp}.getLeftChild);  
-      localStack.push $(eval ${temp}.getRightChild);  
-      if [[ $(eval ${temp}.getRightChild) != "null" ]]||[[ $(eval ${temp}.getLeftChild) != "null" ]];then
-        isRowEmpty="false"
-      fi
-    else
-      echo -n "--"
-      localStack.push "null";
-      localStack.push "null"
-    fi
-    nBlanks2=$((nBlanks*2-2));
-    for ((j=0; j<nBlanks2; j++));do
-      echo -n " ";
-    done
-  done
-  echo ""
-  nBlanks=$((nBlanks/2))
-  while [[ $(localStack.isEmpty) == "false" ]];do
-    globalStack.push $(localStack.peek) 
-    localStack.pop
-  done 
-done
-echo ""......................................................""
-
-}
 # Generates a new node 'object'
 #インスタンスを生成する
 function new_node() {
@@ -135,9 +89,62 @@ function new_node() {
 	local left="$3";
 	local right="$4";
 	eval "${node_id}set='set'";
-	eval "set_node_value $node_id $value";
-	eval "set_node_left $node_id $right";
-	eval "set_node_right $node_id $right";
+	set_node_value "$node_id" "$value";
+	set_node_left "$node_id" "$right";
+	set_node_right "$node_id" "$right";
+}
+###
+#スタックを生成する
+function new_stack(){
+        local stackname="$1";
+        eval "${stackname}arr=()";
+
+}
+##
+function display_Tree(){
+
+Stack_global=();
+Stack_global.push "$root";
+nBlanks=32;
+isRowEmpty="false";
+echo "......................................................"
+while [[ $isRowEmpty == "false" ]];do
+  Stack_local=();
+  isRowEmpty="true"
+  for ((j=0; j<nBlanks; j++));do
+    echo -n " ";
+  done
+  while [[ $(Stack_global.isEmpty) == "false" ]];do
+    #サブシェルでpopメソッドを呼び出しても配列の削除が反映されないので
+    #peekメソッドを別に作った。
+    temp=$(Stack_global.peek);
+    Stack_global.pop
+    if [ "$temp" != "null" ];then
+      echo -n $(${temp}.getValue)
+      Stack_local.push $(eval ${temp}.getLeftChild);  
+      Stack_local.push $(eval ${temp}.getRightChild);  
+      if [[ $(eval ${temp}.getRightChild) != "null" ]]||[[ $(eval ${temp}.getLeftChild) != "null" ]];then
+        isRowEmpty="false"
+      fi
+    else
+      echo -n "--"
+      Stack_local.push "null";
+      Stack_local.push "null"
+    fi
+    nBlanks2=$((nBlanks*2-2));
+    for ((j=0; j<nBlanks2; j++));do
+      echo -n " ";
+    done
+  done
+  echo ""
+  nBlanks=$((nBlanks/2))
+  while [[ $(Stack_local.isEmpty) == "false" ]];do
+    Stack_global.push $(Stack_local.peek) 
+    Stack_local.pop
+  done 
+done
+echo ""......................................................""
+
 }
 function theTree_insert(){
   local value=$1;
